b979c4921934818c5a28e2bb213315ec9e4b890d
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-05-17 13:21:51 +0200

    refakturer

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pensjon.di.dependencies import Dependencies
 from pensjon.lakehouse.config import LakehouseConfig
 from pensjon.lakehouse.pipeline import PensjonLakehousePipeline
 
-
 if __name__ == "__main__":
     pipeline = PensjonLakehousePipeline(
         deps=Dependencies(),
