cad5805d9d42261592d3d8ed330c8c6c2252d569
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-05-19 14:17:05 +0200

    Slett gamle ubrukte filer

--- main.py
+++
@@ -1,7 +0,0 @@
-import sys
-from pensjon.lakehouse.pipeline import PensjonLakehousePipeline
-
-if __name__ == "__main__":
-    use_testdata = "--testdata" in sys.argv
-    pipeline = PensjonLakehousePipeline(use_testdata=use_testdata)
-    pipeline.run()
